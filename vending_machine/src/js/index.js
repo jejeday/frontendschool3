46b38cb8d0f4b3b8b0d2e4dc3ce6f1b2b89471c0
jejeday <jhjae1@naver.com>
2022-11-15 23:58:33 +0900

    221115

---
+++ vending_machine/src/js/index.js
@@ -0,0 +1,16 @@
+import ColaGenerator from "./components/colaGenerator.js";
+import Vendingmachine from "./components/vendingmachine.js";
+
+const colaGenerator = new ColaGenerator();
+const vendingmachine = new Vendingmachine();
+
+// 자바스크립트는 await를 만나면 프라미스 객체가 반환될 때까지 기다립니다.
+// 원래 await는 탑레벨에서 작동하지 않습니다. https://ko.javascript.info/async-await
+// (async () => {
+//    await colaGenerator.setup();
+//    vendingmachineFunc.setup();
+// })()
+
+// 탑레벨 await는 최상위모듈에서만 작동하며, async함수만 사용할 수 있습니다. https://v8.dev/features/top-level-await
+await colaGenerator.setup();
+vendingmachine.setup();
